ba1f7dc16eb7b7fd2b47370681e416d44f529ea7
sxpujs <sxpujs@163.com>
2025-05-05 09:44:49 +0800

    add L283MoveZeroes

---
+++ src/main/java/com/example/L283MoveZeroes.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class L283MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int len = nums.length, left = 0, right = 0;
+        while (right < len) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+    public static void main(String[] args) {
+        L283MoveZeroes moveZeroes = new L283MoveZeroes();
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes.moveZeroes(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
